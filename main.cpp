3737c8a0084a44344ec1a1060765860e14e09c67
Franciszek Juras <franciszekjuras@gmail.com>
2019-02-06 01:39:52 +0100

    Prevent firpm library from increasing kernel rank.

--- main.cpp
+++ main.cpp
@@ -3,24 +3,33 @@
 #include "firker.h"
 
 int main(){
-	LeastSqFirKer ker;
-	ker.setRank(50);
+	EqRippleFirKer ker;
+	ker.setRank(1000);
 	ker.setSampFreq(1000.);
 
-	std::cout << std::boolalpha << ker.setSpecs({100.},{0.,1.}) << std::endl;
-	ker.setWindow(LeastSqFirKer::Window::blackman);
+	ker.setSpecs({100.,101.},{0.,0.,1.,1.},{1.,1.});
 	ker.calc();
-	std::vector<double> v(ker.getKernel());
-	std::fstream fs("ker.txt", std::fstream::out);
-	for(auto i : v){
-		fs << i << " ";
-	}
-	std::vector<double> t(FirKer::toBode(ker.transmission(1000)));
-	std::fstream fs2("trns.txt", std::fstream::out);
-	for(auto i : t){
-		fs2 << i << " ";
-	}
-	std::cout << "Sampling frequency: " << ker.getSampFreq() << " KHz" "\n" <<  "Done" << std::endl;
+	std::cout << "L: " << ker.getKernel().size() << "\n";
+	ker.setRank(999);
+	ker.setSpecs({100.,101.},{0.,0.,1.,1.},{1.,1.});
+	ker.calc();
+	std::cout << "L: " << ker.getKernel().size() << "\n";
+	ker.setSpecs({100.,101.,200.,201.},{0.,0.,1.,1.,0.,0.},{1.,1.,1.});
+	ker.calc();
+	std::cout << "L: " << ker.getKernel().size() << "\n";
+	ker.setSpecs({100.,101.},{1.,1.,0.,0.},{1.,1.});
+	ker.calc();
+	std::cout << "L: " << ker.getKernel().size() << "\n";
+	// std::fstream fs("ker.txt", std::fstream::out);
+	// for(auto i : v){
+	// 	fs << i << " ";
+	// }
+	// std::vector<double> t(FirKer::toBode(ker.transmission(1000)));
+	// std::fstream fs2("trns.txt", std::fstream::out);
+	// for(auto i : t){
+	// 	fs2 << i << " ";
+	// }
+	// std::cout << "Sampling frequency: " << ker.getSampFreq() << " KHz" "\n" <<  "Done" << std::endl;
 
 	return 0;
 }
